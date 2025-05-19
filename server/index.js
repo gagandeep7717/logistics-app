6c443cd172890ae615e831a86736b25455927412
GagandeepSingh Randhawa <gagandeep7717@gmail.com>
2025-05-18 19:45:10 -0700

    cursor changes for railway

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API Running"));
-app.listen(4000, () => console.log("Server running on http://localhost:4000"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
